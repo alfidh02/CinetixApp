1acec85984f816ef9c34ccf69ec73b4a450f85b6
alfidh02 <alfidarmawan79@gmail.com>
2020-05-06 00:05:59 +0700

    Minor changes

--- app/src/main/java/com/koma/cinetixapp/onboarding/PageThreeActivity.kt
+++ app/src/main/java/com/koma/cinetixapp/onboarding/PageThreeActivity.kt
@@ -8,6 +8,6 @@ class PageThreeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_page_one)
+        setContentView(R.layout.activity_page_three)
     }
 }
--- app/src/main/java/com/koma/cinetixapp/onboarding/PageTwoActivity.kt
+++ app/src/main/java/com/koma/cinetixapp/onboarding/PageTwoActivity.kt
@@ -8,6 +8,6 @@ class PageTwoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_page_one)
+        setContentView(R.layout.activity_page_two)
     }
 }
