ba7b86dd22aeee1804f00df527fa707eb1edac3e
alfidh02 <alfidarmawan79@gmail.com>
2020-05-05 23:45:38 +0700

    Add onboarding screen

---
+++ app/src/main/java/com/koma/cinetixapp/onboarding/PageTwoActivity.kt
@@ -0,0 +1,13 @@
+package com.koma.cinetixapp.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koma.cinetixapp.R
+
+class PageTwoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_one)
+    }
+}
