773df0cfee939346fee0a8ce6d1ab3bdedff69de
alfidh02 <alfidarmawan79@gmail.com>
2020-05-05 22:41:37 +0700

    Initial commit

---
+++ app/src/main/java/com/koma/cinetixapp/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.koma.cinetixapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
