b71f73ed969488d2ff8c2f49f1d384ea874d28c3
alfidh02 <alfidarmawan79@gmail.com>
2020-05-14 12:32:35 +0700

    Setting fragment base function

--- app/src/main/java/com/koma/cinetixapp/home/SettingFragment.kt
+++ app/src/main/java/com/koma/cinetixapp/home/SettingFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 import com.koma.cinetixapp.R
+import com.koma.cinetixapp.utils.Preferences
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class SettingFragment : Fragment() {
+
+    lateinit var preferences: Preferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +25,20 @@ class SettingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        preferences = Preferences(context!!.applicationContext)
+
+        iv_nama.text = preferences.getValues("nama")
+        tv_email.text = preferences.getValues("email")
+
+        Glide.with(this)
+            .load(preferences.getValues("url"))
+            .apply(RequestOptions.circleCropTransform())
+            .into(iv_profile)
     }
 
 }
