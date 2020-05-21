9863231ec7e0094234ddce9fc1bb87cdf9101022
alfidh02 <alfidarmawan79@gmail.com>
2020-05-21 23:31:19 +0700

    Top up minor changes

--- app/src/main/java/com/koma/cinetixapp/wallet/TopUp.kt
+++ app/src/main/java/com/koma/cinetixapp/wallet/TopUp.kt
@@ -2,6 +2,7 @@ package com.koma.cinetixapp.wallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.koma.cinetixapp.R
 
 class TopUp : AppCompatActivity() {
@@ -9,5 +10,7 @@ class TopUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_up)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
